87e45b7d06d8084699fb8048de59389b8d03ff9c
brenno <brennoantonios@gmail.com>
2025-01-13 23:53:14 -0400

    config to react netfly

--- src/main/java/todo/todolist/config/WebConfig.java
+++ src/main/java/todo/todolist/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Adiciona CORS para todos os endpoints
-                .allowedOrigins("http://192.168.15.5:3000", "http://192.168.15.5:3000") // Permite acesso de origens específicas (seu frontend)
+                .allowedOrigins(
+                        "https://reactodolistproject.netlify.app",  // Frontend no Netlify
+                        System.getenv("FRONTEND_URL") // Usando variável de ambiente (caso seja configurada no ambiente)
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite os métodos HTTP
                 .allowedHeaders("*") // Permite qualquer cabeçalho
                 .allowCredentials(true); // Permite enviar cookies e autenticação
